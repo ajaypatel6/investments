75d48a6b23489de6165db063d9fa2c544e336141
Ajay <ajaypatel23623@gmail.com>
2022-01-08 15:05:05 -0500

    ok

--- back/Main.js
+++ back/Main.js
@@ -1,30 +1,30 @@
-const express = require('express');
-const app = express();
-const path = require('path')
-
 const express = require("express");
 const app = express();
+const path = require("path");
+
 const cors = require("cors");
 const pool = require("./db");
 
 //middlewares
 app.use(cors());
 app.use(express.json());
-
-app.use(express.static(path.join(__dirname, 'build')))
-
-app.get('/', function(req,res){
-    res.sendFile(path.join(__dirname, 'build', 'index.html' ))
-})
+app.use(express.static(path.join(__dirname, "build")));
 
 
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
 
 //
-app.listen(3000);
+app.listen(3001);
 // app.js running  in exprss
 
 // Dont need cors with this method, because they are running on different ports, to share resoureces
 
-// to edit front end, 
+// to edit front end,
 // make change, then run bpm build again, copy to back end(from front en)
 //     "build": "react-scripts build && xcopy", (windows, auto?)
+
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome " });
+});
